c1cd2cc05b2dfd92cb6e288785da2c8cacaa46d3
t-ueshiba <t.ueshiba@aist.go.jp>
2026-05-06 20:36:25 +0900

    Add license file

--- README.md
+++ README.md
@@ -14,6 +14,28 @@ It consists of two ROS2 packages;
 - [aist_robotiq](./aist_robotiq/): drivers and controllers
 - [aist_robotiq_msgs](./aist_robotiq_msgs/): definitions of ROS2 messages, services and actions used by `aist_robotiq`
 
+## Installation
+First, you should install a developer package of C++ library,  `nlohmann-json3`, for handling `JSON` messages in `C++` code;
+```bash
+sudo apt install nlohmann-json3-dev
+```
+Then download the [source code of this package](https://github.com/Task-Intelligent-Robotics-Research-Grp/aist_robotiq) as well as its dependencies into the ROS2 workspace;
+```bash
+cd ros2_ws/src
+git clone https://github.com/Task-Intelligent-Robotics-Research-Grp/aist_robotiq
+vcs import . --input=aist_robotiq/dependencies.repos
+```
+Finally, you can compile the package by typing                               
+```bash      
+source ros2_ws/install/setup.bash
+colcon build                                                                  
+```                                                                             
+
+
+## Usage
 Please consult `README`s in each package for usage.
+
+
+
 
 
--- aist_robotiq/aist_robotiq/cmodel_urcap.py
+++ aist_robotiq/aist_robotiq/cmodel_urcap.py
@@ -1,8 +1,40 @@
-"""Module for controlling Robotiq's grippers"""
-# BASED ON: https://dof.robotiq.com/discussion/1962/programming-options-ur16e-2f-85#latest
-# Ported to ROS by felixvd
-# Modified by T.Ueshiba
-
+#  BSD 3-Clause License
+#
+#  Copyright (c) 2026, National Institute of Advanced Industrial Science
+#  and Technology(AIST)
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  1. Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.
+#
+#  2. Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+#
+#  3. Neither the name of the copyright holder nor the names of its
+#     contributors may be used to endorse or promote products derived from
+#     this software without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+#  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+#  OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+#  OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+#  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+#  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+#  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+#  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#  Author: Toshio Ueshiba (t.ueshiba@aist.go.jp)
+#
+#  BASED ON: https://dof.robotiq.com/discussion/1962/programming-options-ur16e-2f-85#latest
+#  Ported to ROS by felixvd
+#  Modified by T.Ueshiba
+#
 import time, socket, threading
 from aist_robotiq.cmodel_base import CModelBase
 from aist_robotiq_msgs.msg    import CModelStatus
